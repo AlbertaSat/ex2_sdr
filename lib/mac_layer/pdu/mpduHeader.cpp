56342a0670fe17adbf7f2e15054f4dd22b9f082f
StevenKnudsen <knud@ualberta.ca>
2022-05-18 17:32:46 -0600

    revert endianess changes for mpdu header.

--- lib/mac_layer/pdu/mpduHeader.cpp
+++ lib/mac_layer/pdu/mpduHeader.cpp
@@ -16,6 +16,30 @@
 #include "mpdu.hpp"
 #include "rfMode.hpp"
 
+<<<<<<< HEAD
+=======
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
+    defined(__BIG_ENDIAN__) || \
+    defined(__ARMEB__) || \
+    defined(__THUMBEB__) || \
+    defined(__AARCH64EB__) || \
+    defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
+// It's a big-endian target architecture
+#define LITTLE_ENDIAN_ARCH 0
+//#include <arpa/inet.h>
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
+    defined(__LITTLE_ENDIAN__) || \
+    defined(__ARMEL__) || \
+    defined(__THUMBEL__) || \
+    defined(__AARCH64EL__) || \
+    defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
+// It's a little-endian target architecture
+#define LITTLE_ENDIAN_ARCH 1
+#else
+#error "I don't know what architecture this is!"
+#endif
+
+>>>>>>> a88b8dc (Accomodate TI C++ compiler)
 namespace ex2 {
   namespace sdr {
 
