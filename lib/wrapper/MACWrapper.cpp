a767d33b6782490504213c0d3b40db5b701e3082
Ron Unrau <runrau@Rons-MacBook-Pro.local>
2022-06-03 12:12:23 -0600

    MACWrapper.cpp: replace malloc with os_malloc

--- lib/wrapper/MACWrapper.cpp
+++ lib/wrapper/MACWrapper.cpp
@@ -15,7 +15,7 @@
 
 #include "mac.hpp"
 #include "mpdu.hpp"
-
+#include "osal.h"
 
 struct mac {
   void *obj;
@@ -40,7 +40,6 @@ void mac_destroy(mac_t *m)
 
   delete static_cast<ex2::sdr::MAC *>(m->obj);
   delete m;
-  m = NULL; // @todo not sure this really does anything that matters
 }
 
 // ErrorCorrection::ErrorCorrectionScheme getErrorCorrectionScheme ();
