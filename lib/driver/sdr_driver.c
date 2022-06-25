6ef1ef2af443e039d339afbd90cc50f315cf4c0e
Ron Unrau <rcunrau@ualberta.ca>
2022-06-24 19:22:46 -0600

    sdr_driver.c: merge PIPE mode stuff from master

--- lib/driver/sdr_driver.c
+++ lib/driver/sdr_driver.c
@@ -1,6 +1,10 @@
+#include <string.h>
 #include "sdr_driver.h"
 #include "fec.h"
 #include "osal.h"
+
+#define PIPE_ENTER_MSG_LEN 15
+#define PIPE_EXIT_MSG_LEN 16
 
 /* From the EnduroSat manual, these delays assume the UART speed is 19.2KBaud */
 static int sdr_uhf_baud_rate_delay[] = {
@@ -8,7 +12,8 @@ static int sdr_uhf_baud_rate_delay[] = {
     [SDR_UHF_2400_BAUD] = 460,
     [SDR_UHF_4800_BAUD] = 240,
     [SDR_UHF_9600_BAUD] = 120,
-    [SDR_UHF_19200_BAUD] = 60
+    [SDR_UHF_19200_BAUD] = 60,
+    [SDR_UHF_TEST_BAUD] = 20
 };
 
 int sdr_uhf_tx(sdr_interface_data_t *ifdata, uint8_t *data, uint16_t len) {
@@ -40,6 +45,21 @@ void sdr_rx_isr(void *cb_data, uint8_t *buf, size_t len, void *pxTaskWoken) {
             ifdata->rx_mpdu_index = 0;
             // This is an isr, if this fails there's nothing that can be done
             os_queue_enqueue(ifdata->rx_queue, ifdata->rx_mpdu);
+        }
+    }
+
+    //Discard transceiver pipe mode status updates
+    //Could probably be done differently to use less cpu
+    if (ifdata->rx_mpdu_index == (PIPE_ENTER_MSG_LEN)) {
+        char pipe_enter_msg[PIPE_ENTER_MSG_LEN] = {'+', 'P', 'I', 'P', 'E', ' ', '7', '8', '7', '8', 'B', '8', 'A', 'E', 0x0D};
+        if(strncmp((char *)ifdata->rx_mpdu, pipe_enter_msg, PIPE_ENTER_MSG_LEN) == 0){
+            ifdata->rx_mpdu_index = 0;
+        }
+    }
+    if (ifdata->rx_mpdu_index == (PIPE_EXIT_MSG_LEN)) {
+        char pipe_exit_msg[PIPE_EXIT_MSG_LEN] = {'+', 'E', 'S', 'T', 'T', 'C', ' ', 'C', 'F', 'B', '5', '2', 'D', '3', '5', 0x0D};
+        if (strncmp((char *)(ifdata->rx_mpdu), pipe_exit_msg, PIPE_EXIT_MSG_LEN) == 0) {
+            ifdata->rx_mpdu_index = 0;
         }
     }
 }
